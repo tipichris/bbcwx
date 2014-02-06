0e562bab2fedf6e7305da6f63e659e2813fa6d0a
Chris Hastie <chris@oak-wood.co.uk>
2014-02-06 18:05:28 +0000

    License

--- desklet.js
+++ desklet.js
@@ -1,4 +1,25 @@
-
+/* 
+ * bbcwx - a Cinnamon Desklet displaying the weather retrieved
+ * from the BBC's RSS feed.
+ * 
+ * Copyright 2014 Chris Hastie. Forked from accudesk@logan; original
+ * code Copyright loganj. Includes the marknote library, Copyright
+ * 2011 jbulb.org. Icons Copyright Merlin the Red. See help.html
+ * for further credits and license information.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 const Gio = imports.gi.Gio;
 const St = imports.gi.St;
