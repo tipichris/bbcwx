758ba1ce8f45e85a1a86275593eea0da265e2152
Chris Hastie <chris@oak-wood.co.uk>
2014-02-22 18:10:04 +0000

    Change threshold vis for 1 decimal place

--- desklet.js
+++ desklet.js
@@ -768,7 +768,7 @@ MyDesklet.prototype = {
     }
     let km = 1*vis;
     let out = km * conversion[this.wunits];
-    let decpl = (out < 3) ? 1 : 0;
+    let decpl = (out < 4) ? 1 : 0;
     out = out.toFixed(decpl);
     if (units) {
       out += unitstring[this.wunits];
